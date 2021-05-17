0cb04f8ce07928f84b1257faa37e011b1a3d390f
Robyn Taylor <rhtaylor0715@gmail.com>
2021-05-17 17:07:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,14 @@ class Dog
   attr_accessor :name
   
   def save
-    
+    @@all << self
+  end
   
   def initialize(name)
     @name = name
+    save
   end
   
-  
-    
   def self.all
     @@all
   end
